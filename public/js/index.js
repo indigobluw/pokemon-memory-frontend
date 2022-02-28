7979ad339fe6407565d0e00186341c46b89c3a6c
indigobluw <lindquist20@gmail.com>
2022-02-28 15:00:24 +0100

    connect to json

--- public/js/index.js
+++ public/js/index.js
@@ -1,140 +1,31 @@
-/*console.log("Hej på dig")
-var anElementet = document.getElementById("app")
-
-//appElementet.innerHTML="Hello World"
-//appElementet.outerHTML="Hello World"
-
-var anElement = document.createElement("h1")
-appElementet.appendChild(ettElement)
-var app = document.getElementById("app")
-app.appendChild(an)
-
-setTimeout(age, 3000)
-
-function age() { 
-    console.log("Ageing game")
-    setTimeout(age, 3000)
-}
-age()
-
-function age() {
-    var xhr = new XMLHttpRequest()
-    //xhr.open("GET", "/js/data.json") 
-    xhr.open("GET", "http://localhost:3000")
-    xhr.onload=function() { 
-        var data = JSON.parse(this.response)
-        createTable(data)
-    }
-    xhr.send()
-}
-
-function createTable(data) { 
-    var appElementet = document.getElementById("app")
-    var aTable = document.createElement("table")
-    appElementet.appendChild(aTable)
-    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].logo))
-    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].logo))
-    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].logo))
-    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].logo))
-}
-
-function createRow(name, points, url) { 
-    var aRow = document.createElement("tr")
-    aRow.appendChild(createCell(name))
-    aRow.appendChild(createCell(points))
-    aRow.appendChild(createCells(url))
-
-    return aRow
-}
-
-function createCell(content){ 
-    var aCell = document.createElement("td") 
-    aCell.innerHTML = content 
-    return aCell;
-}
-function createImageCell(url){
-    var aCell = document.createElement("td")
-    var anImage = document.createElement("img")
-    anImage.src=url
-    anImage.classList.add("tableLogo")
-    aCell.appendChild(anImage)
-    return aCell;
-}*/
-
+console.log("hej");
 const section = document.querySelector("section"); 
 const playerLivesCount = document.querySelector("span"); 
 let playerLives = 8; //how many tries the player's got 
+let myData = [];
+
+const generateGetDate = () => {
+    let xhr = new XMLHttpRequest()
+    xhr.open("GET", "/js/data.json", false)
+    xhr.send();
+    let data = JSON.parse(xhr.response); 
+    myData = []; 
+    for ( i = 0; i<8; i++) {
+        myData.push(data[i]);
+        myData.push(data[i]);
+    }
+    console.log("AJAX run");
+    console.log(myData);
+}
 
 //link text
 playerLivesCount.textContent = playerLives;
 
 //generate the data for the cards. [{}] = array of objects
-const getData = () => [
-    {
-        imgSrc: "./image/chikorita.png",
-        name: "chikorita"
-    },
-    { 
-        imgSrc: "./image/dragonite.png",
-        name: "dragonite"
-    },
-    {
-        imgSrc:"./image/eevee.png",
-        name: "eevee"
-    },
-    {
-        imgSrc:"./image/groudon.png",
-        name: "groudon"
-    },
-    {
-        imgSrc:"./image/gyrados.png",
-        name: "gyrados"
-    },
-    {
-        imgSrc:"./image/lugia.png",
-        name: "lugia"
-    },
-    {
-        imgSrc:"./image/metagross.png",
-        name: "metagross"
-    },
-    {
-        imgSrc:"./image/milotic.png",
-        name: "milotic"
-    },
-    {
-        imgSrc: "./image/chikorita.png",
-        name: "chikorita"
-    },
-    { 
-        imgSrc: "./image/dragonite.png",
-        name: "dragonite"
-    },
-    {
-        imgSrc:"./image/eevee.png",
-        name: "eevee"
-    },
-    {
-        imgSrc:"./image/groudon.png",
-        name: "groudon"
-    },
-    {
-        imgSrc:"./image/gyrados.png",
-        name: "gyrados"
-    },
-    {
-        imgSrc:"./image/lugia.png",
-        name: "lugia"
-    },
-    {
-        imgSrc:"./image/metagross.png",
-        name: "metagross"
-    },
-    {
-        imgSrc:"./image/milotic.png",
-        name: "milotic"
-    }
-];
+
+function getData() { 
+    return myData;
+}
 
 // => arrow function
 const randomize = () => {
@@ -223,5 +114,5 @@ const restart = (text) => {
         window.alert(text, 100);
     });
 };
-
+generateGetDate();
 cardGenerator();
